8e7e18299171b96e13983c856c1c123705f11351
Ash Bellett <ash.bellett.ab@gmail.com>
2019-09-28 15:53:02 +1000

    Changed config variables to constants

--- mdp/lib/config.py
+++ mdp/lib/config.py
@@ -2,73 +2,66 @@ FIGURE_DIRECTORY = './mdp/output/graph.png'
 EPISODES = 1000
 HORIZON = 100
 
-states = ['a', 'b', 'c']            # state space
-actions = ['x', 'y']                # action space
-rewards = {'a': 0, 'b': 1, 'c': -1} # reward function
-discount_factor = 1
+STATES = ['a', 'b', 'c']            # state space
+ACTIONS = ['x', 'y']                # action space
+REWARDS = {'a': 0, 'b': 1, 'c': -1} # reward function
+DISCOUNT_FACTOR = 1
 
-policy = {}
-transitions = {}
-probabilities = {}
+POLICY = {}
+TRANSITIONS = {}
+LABELS = {}
 
 # Set action probabilities (policy)
-# policy[state, action]
-policy['a', 'x'] = 2/3
-policy['a', 'y'] = 1/3
-policy['b', 'x'] = 2/3
-policy['b', 'y'] = 1/3
-policy['c', 'x'] = 2/3
-policy['c', 'y'] = 1/3
-
-# policy['a', 'x'] = 1/2
-# policy['a', 'y'] = 1/2
-# policy['b', 'x'] = 1/2
-# policy['b', 'y'] = 1/2
-# policy['c', 'x'] = 1/2
-# policy['c', 'y'] = 1/2
+# POLICY[state, action]
+POLICY['a', 'x'] = 2/3
+POLICY['a', 'y'] = 1/3
+POLICY['b', 'x'] = 2/3
+POLICY['b', 'y'] = 1/3
+POLICY['c', 'x'] = 2/3
+POLICY['c', 'y'] = 1/3
 
 # Set state transition probabilities
-# transitions[(state, next_state), action]
-transitions[('a', 'a'), 'x'] = 0
-transitions[('a', 'a'), 'y'] = 0
-transitions[('b', 'b'), 'x'] = 0
-transitions[('b', 'b'), 'y'] = 0
-transitions[('c', 'c'), 'x'] = 0
-transitions[('c', 'c'), 'y'] = 0
-transitions[('a', 'b'), 'x'] = 2/3
-transitions[('a', 'b'), 'y'] = 1/3
-transitions[('a', 'c'), 'x'] = 1/3
-transitions[('a', 'c'), 'y'] = 2/3
-transitions[('b', 'a'), 'x'] = 1/3
-transitions[('b', 'a'), 'y'] = 2/3
-transitions[('b', 'c'), 'x'] = 2/3
-transitions[('b', 'c'), 'y'] = 1/3
-transitions[('c', 'a'), 'x'] = 2/3
-transitions[('c', 'a'), 'y'] = 1/3
-transitions[('c', 'b'), 'x'] = 1/3
-transitions[('c', 'b'), 'y'] = 2/3
+# TRANSITIONS[(state, next_state), action]
+TRANSITIONS[('a', 'a'), 'x'] = 0
+TRANSITIONS[('a', 'a'), 'y'] = 0
+TRANSITIONS[('b', 'b'), 'x'] = 0
+TRANSITIONS[('b', 'b'), 'y'] = 0
+TRANSITIONS[('c', 'c'), 'x'] = 0
+TRANSITIONS[('c', 'c'), 'y'] = 0
+TRANSITIONS[('a', 'b'), 'x'] = 2/3
+TRANSITIONS[('a', 'b'), 'y'] = 1/3
+TRANSITIONS[('a', 'c'), 'x'] = 1/3
+TRANSITIONS[('a', 'c'), 'y'] = 2/3
+TRANSITIONS[('b', 'a'), 'x'] = 1/3
+TRANSITIONS[('b', 'a'), 'y'] = 2/3
+TRANSITIONS[('b', 'c'), 'x'] = 2/3
+TRANSITIONS[('b', 'c'), 'y'] = 1/3
+TRANSITIONS[('c', 'a'), 'x'] = 2/3
+TRANSITIONS[('c', 'a'), 'y'] = 1/3
+TRANSITIONS[('c', 'b'), 'x'] = 1/3
+TRANSITIONS[('c', 'b'), 'y'] = 2/3
 
-# probabilities[state, state-action]
-# probabilities[state-action, state]
-probabilities['a', ('a','x')] = '2/3'
-probabilities['a', ('a','y')] = '1/3'
-probabilities['b', ('b','x')] = '2/3'
-probabilities['b', ('b','x')] = '2/3'
-probabilities['b', ('b','y')] = '1/3'
-probabilities['c', ('c','x')] = '2/3'
-probabilities['c', ('c','y')] = '1/3'
-probabilities[('a','x'), 'b'] = '2/3'
-probabilities[('a','x'), 'c'] = '1/3'
-probabilities[('a','y'), 'b'] = '1/3'
-probabilities[('a','y'), 'c'] = '2/3'
-probabilities[('b','x'), 'a'] = '1/3'
-probabilities[('b','x'), 'c'] = '2/3'
-probabilities[('b','y'), 'a'] = '2/3'
-probabilities[('b','y'), 'c'] = '1/3'
-probabilities[('c','x'), 'a'] = '2/3'
-probabilities[('c','x'), 'b'] = '1/3'
-probabilities[('c','y'), 'a'] = '1/3'
-probabilities[('c','y'), 'b'] = '2/3'
+# LABELS[state, state-action]
+# LABELS[state-action, state]
+LABELS['a', ('a','x')] = '2/3'
+LABELS['a', ('a','y')] = '1/3'
+LABELS['b', ('b','x')] = '2/3'
+LABELS['b', ('b','x')] = '2/3'
+LABELS['b', ('b','y')] = '1/3'
+LABELS['c', ('c','x')] = '2/3'
+LABELS['c', ('c','y')] = '1/3'
+LABELS[('a','x'), 'b'] = '2/3'
+LABELS[('a','x'), 'c'] = '1/3'
+LABELS[('a','y'), 'b'] = '1/3'
+LABELS[('a','y'), 'c'] = '2/3'
+LABELS[('b','x'), 'a'] = '1/3'
+LABELS[('b','x'), 'c'] = '2/3'
+LABELS[('b','y'), 'a'] = '2/3'
+LABELS[('b','y'), 'c'] = '1/3'
+LABELS[('c','x'), 'a'] = '2/3'
+LABELS[('c','x'), 'b'] = '1/3'
+LABELS[('c','y'), 'a'] = '1/3'
+LABELS[('c','y'), 'b'] = '2/3'
 
 # # Node labels:
 # {
--- mdp/main.py
+++ mdp/main.py
@@ -3,24 +3,28 @@ from lib.agent import Agent
 from lib.episode import Episode
 from lib.graph import Graph
 
-# Print expected rewards and transition probabilities
-agent = Agent(states, actions, rewards, HORIZON, discount_factor, policy, transitions)
+# Predict expected return for each time step
+agent = Agent(STATES, ACTIONS, REWARDS, HORIZON, DISCOUNT_FACTOR, POLICY, TRANSITIONS)
 expected_return_under_policy = agent.expected_return_under_policy()
-expected_rewards = {}
-for state in states:
-    expected_rewards[state] = round(expected_return_under_policy[state, HORIZON-1])
-print('Expected rewards:', expected_rewards)
 
+# Get expected rewards at end of episode
+expected_rewards = {}
+for state in STATES:
+    expected_rewards[state] = round(expected_return_under_policy[state, HORIZON-1])
+
+# Simulate an episode to get actual rewards
 actual_rewards = {}
-for state in states:
-    episode = Episode(actions, states, rewards, HORIZON, policy, transitions, state)
+for state in STATES:
+    episode = Episode(ACTIONS, STATES, REWARDS, HORIZON, POLICY, TRANSITIONS, state)
     actual_rewards[state] = 0
     for _ in range(EPISODES):
         actual_rewards[state] += episode.play()
     actual_rewards[state] /= EPISODES
 
+# Compare predicted vs. actual rewards
+print('Expected rewards:', expected_rewards)
 print('Actual rewards:  ', actual_rewards)
 
 # Draw state-action diagram
-#graph = Graph(states, actions, probabilities)
-#graph.draw()
+graph = Graph(STATES, ACTIONS, LABELS)
+graph.draw()
